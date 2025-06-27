82cf100aaa5a4e026aabe35d67acf920ce07c315
UnderStudy <yuta.takegami@gmail.com>
2025-06-27 22:03:15 +0900

    test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,28 @@
 use url::percent_encoding::percent_decode;
+use std::env;
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let args: Vec<String> = env::args().collect();
     let input = &args[1];
 
     //let input = "foo%20bar";
     let decoded = percent_decode(input.as_bytes()).decode_utf8();
-    println!("{}", decoded.unwrap());
+    println!("{}", decode(input));
+}
+
+fn decode(input: &str) -> String{
+    percent_decode(input.as_bytes()).decode_utf8().unwrap().to_string()
+}
+
+# [cfg(test)]
+mod tests {
+    use crate::decode;
+
+    #[test]
+    fn decode_space_ok(){
+        let expected = "foo bar";
+        let input = "foo%20bar";
+        let actual = decode(input);
+        assert_eq!(expected, actual);
+    }
 }
