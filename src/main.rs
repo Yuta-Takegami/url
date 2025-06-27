938fa875eb74c5140289e5300a20d332c19517f4
UnderStudy <yuta.takegami@gmail.com>
2025-06-27 19:42:45 +0900

    バージョン変更

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use url::percent_encoding::percent_decode;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let input = &args[1];
+
+    //let input = "foo%20bar";
+    let decoded = percent_decode(input.as_bytes()).decode_utf8();
+    println!("{}", decoded.unwrap());
 }
